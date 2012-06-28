ba8c2f42c2d9c5a05e9915906054f76b2603a98b
TamCore <philipp@tamcore.eu>
2012-06-28 21:01:28 +0200

    acpi: add acpu_check_khz_value(); from bricked
    Conflicts:
    
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -24,6 +24,19 @@
 
 #define CPUFREQ_NAME_LEN 16
 
+<<<<<<< HEAD
+=======
+extern uint32_t acpu_check_khz_value(unsigned long khz);
+
+#ifdef CONFIG_CPU_FREQ_GOV_BADASS_GPU_CONTROL
+/* Badass gpu state detection */
+extern bool gpu_busy_state;
+#endif
+#ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
+/* Intellidemand screen state detection */
+extern bool lmf_screen_state;
+#endif
+>>>>>>> 50554d2... acpi: add acpu_check_khz_value(); from bricked
 
 /*********************************************************************
  *                     CPUFREQ NOTIFIER INTERFACE                    *
