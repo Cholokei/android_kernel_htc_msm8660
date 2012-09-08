812b1462ca4a573e5e1fa69aa8aeac1f3f363df8
Emmanuel Utomi <emmanuelutomi@gmail.com>
2012-09-08 01:07:02 -0500

    tun.c: Rebase with compaitable version

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1237,15 +1237,9 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 	struct ifreq ifr;
 	int sndbuf;
 	int vnet_hdr_sz;
-	int ret; 
+	int ret;
 
-#ifdef CONFIG_ANDROID_PARANOID_NETWORK
-	if (cmd != TUNGETIFF && !capable(CAP_NET_ADMIN)) {
-		return -EPERM;
-	}
-#endif
-
-	if (cmd == TUNSETIFF || _IOC_TYPE(cmd) == 0x89) {
+	if (cmd == TUNSETIFF || _IOC_TYPE(cmd) == 0x89)
 		if (copy_from_user(&ifr, argp, ifreq_len))
 			return -EFAULT;
 
